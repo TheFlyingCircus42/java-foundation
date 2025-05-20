3ae1224ef07e253f9dad31a6b30e2c0decb70d64
Rob Hawkinse <rob.hawkins.89@gmail.com>
2025-05-20 15:33:08 +0100

    more excersise work in JS

--- javascript.js
+++ javascript.js
@@ -2,43 +2,55 @@
 let name = "John";
 let surname = "Doe";
 let age = 11;
-
 age = 54;
-
-
-
-console.log(name);
-console.log(surname);
-console.log(age);
-
-
 cost pi = 3.14
 pi = 10
-
 console.log(pi)
-*/
 
-/*
 let a = 10
-
 console.log (a)
-
 a = 7
-
 console.log (a)
 
 let b = 7 * a
  console.log (b)
+ const max = 57
+ const actual = (max -13)
+ const percentage = (actual/max)
+ console.log (percentage)
  */
 
- const max = 57
- 
- const actual = (max -13)
+//////////////////////////////////////////////////////////
+ // https://javascript.info/variables#variable-naming
 
- const percentage = (actual/max)
+/*
+ let message;
+message = "hello world"; // store the message "hello..." as a string
 
- console.log (percentage)
+alert(message); // shows the variable content
+console.log (message); // shows message in console log
+*/
+/*
+let message = 'Hello!'; // define variabe and content
+alert(message);  // (message) content appears when page opened
 
- 
 
- 
+let user = "john";
+let age = 25;
+let message = "hello!";
+
+//OR
+
+let user = 'John';
+    age = 25;
+    message = 'hello!';
+
+// OR 'comma firts method'
+
+let user = 'John'
+    , age = 25
+    , message = 'Hello!';
+*/
+
+
+
