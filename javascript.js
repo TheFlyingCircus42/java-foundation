30b7b69aaf752395974a1741003f2a02d4d6fdab
Rob Hawkinse <rob.hawkins.89@gmail.com>
2025-05-20 15:19:08 +0100

    more excersise work in JS

---
+++ javascript.js
@@ -0,0 +1,44 @@
+/* 
+let name = "John";
+let surname = "Doe";
+let age = 11;
+
+age = 54;
+
+
+
+console.log(name);
+console.log(surname);
+console.log(age);
+
+
+cost pi = 3.14
+pi = 10
+
+console.log(pi)
+*/
+
+/*
+let a = 10
+
+console.log (a)
+
+a = 7
+
+console.log (a)
+
+let b = 7 * a
+ console.log (b)
+ */
+
+ const max = 57
+ 
+ const actual = (max -13)
+
+ const percentage = (actual/max)
+
+ console.log (percentage)
+
+ 
+
+ 
